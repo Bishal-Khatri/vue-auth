248eb8cef13c4d15c23bce305bbda251c2bb62fb
Bishal Khatri <bishal.khatri343@gmail.com>
2019-05-25 09:09:51 +0545

    router and store managed

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from './axios-auth'
+import axios from '../axios-auth'
 import globalAxios from 'axios'
-import router from './router'
+import router from '../router/'
 
 Vue.use(Vuex)
 
